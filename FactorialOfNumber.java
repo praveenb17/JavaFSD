6b02c9970f1a646245d6f1fcccdbf7631540ed88
TechB <55881022+praveenb17@users.noreply.github.com>
2020-01-26 09:21:40 +0530

    Update FactorialOfNumber.java

--- FactorialOfNumber.java
+++ FactorialOfNumber.java
@@ -1,5 +1,15 @@
 class FactorialOfNum{
 	public static void main(String[] args) {
-		System.out.println("hihihihih");
+	
+		int a=Integer.parseInt(string[0]);
+
+		int fact=1;
+		for(int i=1;i<=a;i++)
+		{
+			fact*=i;
+		}
+
+		System.out.println("Factorial of a number" + a + "is :" + fact);
+
 	}
 }
