fbc6a6a185097b1ba1f92b42f0225ac6c7f14c7d
TechB <55881022+praveenb17@users.noreply.github.com>
2020-01-29 10:44:35 +0530

    Add files via upload

---
+++ tutorials/ArraysDemo.java
@@ -0,0 +1,23 @@
+//arrays
+
+	//syntax
+		//Type []identifier = new Type[Dimension];
+
+	//Example
+		char []ch = new char[10];
+
+		for(int i = 0; i < ch.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+
+		//another way
+
+		Type []identifier = {val1,val2,val3};
+
+		double []dr = {24,345,64};
+
+		//another way
+
+		new int[] {34,4236,246,54765} //anonymous array, it works only with functions
+		
